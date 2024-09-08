a9984e119b3da600a368dfc6b53c3b1b41f96b3f
Tony David <70279376+handleryouth@users.noreply.github.com>
2024-09-08 07:29:03 +0700

    Initial Commit

---
+++ Job Listing/Job_ListingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Job_ListingApp.swift
+//  Job Listing
+//
+//  Created by Tony Gultom on 08/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Job_ListingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
